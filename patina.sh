cb5c4c2e7e572afd5fd2ac5b82e2eec02c403ecd
William Willis Whinn <w_willis86@hotmail.co.uk>
2017-11-13 18:41:02 +0000

    Separate colours and styling to a component

--- patina.sh
+++ patina.sh
@@ -30,33 +30,6 @@ readonly patina_path_components_user="$patina_path_components/user"
 readonly patina_file_configuration="$patina_path_root/patina_configuration.conf"
 readonly patina_file_source="$patina_path_root/patina.sh"
 
-# Text / Colors
-readonly blue='\e[34m'
-readonly cyan='\e[36m'
-readonly green='\e[32m'
-readonly magenta='\e[35m'
-readonly red='\e[31m'
-readonly yellow='\e[33m'
-
-readonly light_blue='\e[94m'
-readonly light_cyan='\e[96m'
-readonly light_gray='\e[37m'
-readonly light_green='\e[92m'
-readonly light_magenta='\e[95m'
-readonly light_red='\e[91m'
-readonly light_yellow='\e[93m'
-
-readonly black='\e[30m'
-readonly gray='\e[90m'
-readonly white='\e[97m'
-
-# Text / Formatting
-readonly bold='\e[1m'
-readonly underline='\e[4m'
-
-readonly color_default='\e[39m'
-readonly color_reset='\e[0m'
-
 #############
 # Functions #
 #############
