5d643a0ebbf15d55d95dd632e43967c80c5d636c
William Willis Whinn <w_willis86@hotmail.co.uk>
2017-11-15 12:45:01 +0000

    Include 'ufw' component

---
+++ components/applications/patina_applications_ufw.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+##############
+# Directives #
+##############
+
+# Color code variables are defined in 'patina.sh'
+# shellcheck disable=SC2154
+
+#############
+# Variables #
+#############
+
+readonly patina_file_ufw_help="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/patina_applications_ufw_help.txt"
+
+#############
+# Functions #
+#############
+
+patina_ufw_configure() {
+  # Failure: Package 'ufw' is not installed
+  if ( ! hash 'ufw' ) ; then
+    echo_wrap "Patina cannot configure the firewall because package 'ufw' is not installed."
+
+  # Failure: Patina has not been given an argument
+  elif [ "$#" -eq "0" ] ; then
+    echo_wrap "Patina has not been given an argument, please try again."
+
+  # Failure: Patina has been given multiple arguments
+  elif [ "$#" -gt "1" ] ; then
+    echo_wrap "Patina must be given only one argument, please try again."
+
+  # Success: Patina has been given a single argument
+  elif [ "$1" ] ; then
+    case "$1" in
+      'disable') sudo ufw disable ;;
+      'enable') sudo ufw enable ;;
+      'help') patina_ufw_help ;;
+      'reset')
+        sudo ufw enable
+        sudo ufw delete allow from 192.168.0.0/24
+        sudo ufw delete limit ssh
+        ;;
+      'setup')
+        sudo ufw enable
+        sudo ufw default deny
+        sudo ufw allow from 192.168.0.0/24
+        sudo ufw limit ssh
+        ;;
+      'status') sudo ufw status ;;
+      *)
+        echo_wrap "Patina requires one of the following arguments: 'enable', 'disable', 'help', 'reset', 'setup', or 'status'."
+        ;;
+    esac
+
+  # Failure: Catch any other error condition here
+  else
+    echo_wrap "Patina has encountered an unexpected error."
+  fi
+}
+
+patina_ufw_help() {
+  clear
+  echo_wrap "${underline}${patina_major_color}Patina / ufw Instructions${color_reset}\\n"
+  echo_wrap "$(cat "${patina_file_ufw_help}")\\n"
+}
+
+###########
+# Aliases #
+###########
+
+alias 'p-ufw'='patina_ufw_configure'
+
+# End of File.
