95853f41b76841f3ff8aa2cd80209bbb8c3c27c7
William Willis Whinn <w_willis86@hotmail.co.uk>
2018-02-01 20:56:04 +0000

    Include patina_commands.sh

---
+++ patina_commands.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+###########
+# Aliases #
+###########
+
+# Components
+alias 'p-list'='patina_list_connected_components'
+
+# Theming
+alias 'p-theme'='patina_theme_apply'
+
+# Reset / Reload
+alias 'p-refresh'='patina_terminal_refresh'
+alias 'p-reset'='patina_terminal_reset'
+
+# End of File.
