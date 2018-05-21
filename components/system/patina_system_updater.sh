7176362b92121827ffcb4284c9666298ff2818df
William Whinn <w.willis.1986@gmail.com>
2018-05-21 19:07:26 +0100

    Update patina_system_updater.sh

--- components/system/patina_system_updater.sh
+++ components/system/patina_system_updater.sh
@@ -17,24 +17,29 @@ patina_update() {
   # Failure: Package 'git' is not installed
   if ( ! hash 'git' ) ; then
     patina_throw_exception 'PE0006'
+    return
 
   # Failure: Patina root is not a 'git' repository
   elif ( ! git -C "$patina_path_root" rev-parse ) ; then
     patina_throw_exception 'PE0009'
+    return
 
   # Failure: Patina is not connected to the Internet
   elif [ "$patina_has_internet" = 'false' ] ; then
     patina_throw_exception 'PE0008'
+    return
 
   # Success: Patina will check for newer 'git' commits
   elif ( git -C "$patina_path_root" rev-parse ) ; then
     cd "$patina_path_root" || return
     git pull
     cd ~- || return
+    return
 
   # Failure: Catch any other error condition here
   else
     patina_throw_exception 'PE0000'
+    return
   fi
 }
 
