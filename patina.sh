8a4d65ac7445af527781a5c2433368b525935ebe
William Whinn <w.willis.1986@gmail.com>
2019-04-02 16:19:28 +0100

    Update structure

--- patina.sh
+++ patina.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-##########
-# Notice #
-##########
+################################################################################
+# Notice #######################################################################
+################################################################################
 
 # Patina: A 'patina', 'layer', or 'toolbox' for BASH under Linux.
 # Copyright (C) 2019 William Willis Whinn
@@ -20,9 +20,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-##############
-# Directives #
-##############
+################################################################################
+# ShellCheck Directives ########################################################
+################################################################################
 
 # Override SC1090: "Can't follow non-constant source".
 # shellcheck source=/dev/null
@@ -30,39 +30,16 @@
 # Override SC2154: "var is referenced but not assigned".
 # shellcheck disable=SC2154
 
-#############
-# Variables #
-#############
+################################################################################
+# Global Variables #############################################################
+################################################################################
 
-# Patina / Metadata
+# Patina / Core / Metadata
 readonly patina_metadata_version='0.7.5'
 readonly patina_metadata_codename='Duchess'
 readonly patina_metadata_url='https://github.com/ultraviolet-1986/patina'
 
-# Patina / Root
-readonly patina_path_root="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
-readonly patina_path_components="$patina_path_root/components"
-readonly patina_path_resources="$patina_path_root/resources"
-
-# Patina / Components
-readonly patina_path_components_applications="$patina_path_components/applications"
-readonly patina_path_components_places="$patina_path_components/places"
-readonly patina_path_components_system="$patina_path_components/system"
-readonly patina_path_components_user="$patina_path_components/user"
-
-# Patina / Resources
-readonly patina_path_configuration="$HOME/.config/patina"
-readonly patina_path_resources_help="$patina_path_resources/help"
-
-# Patina / Files
-readonly patina_file_configuration="$patina_path_configuration/patina.conf"
-readonly patina_file_source="${BASH_SOURCE[0]}"
-
-# System / Files
-readonly system_os_release='/etc/os-release'
-readonly system_lsb_release='/etc/lsb-release'
-
-# Patina / Exceptions
+# Patina / Core / Exceptions
 export readonly PE0000='PE0000: Patina has encountered an unknown error.'
 export readonly PE0001='PE0001: Patina has not been given an expected argument.'
 export readonly PE0002='PE0002: Patina has been given too many arguments.'
@@ -75,16 +52,37 @@ export readonly PE0008='PE0008: Patina does not have access to the Internet.'
 export readonly PE0009='PE0009: Patina cannot detect a valid source control repository.'
 export readonly PE0010='PE0010: Patina cannot access a required variable.'
 
-#############
-# Functions #
-#############
+# Patina / Paths / Root
+readonly patina_path_root="$( cd "$(dirname "${BASH_SOURCE[0]}")" && pwd )"
+readonly patina_path_components="$patina_path_root/components"
+readonly patina_path_resources="$patina_path_root/resources"
+
+# Patina / Paths / Configuration
+readonly patina_path_configuration="$HOME/.config/patina"
+readonly patina_file_configuration="$patina_path_configuration/patina.conf"
+readonly patina_file_source="${BASH_SOURCE[0]}"
+
+# Patina / Paths / Components
+readonly patina_path_components_applications="$patina_path_components/applications"
+readonly patina_path_components_places="$patina_path_components/places"
+readonly patina_path_components_system="$patina_path_components/system"
+readonly patina_path_components_user="$patina_path_components/user"
+
+# Patina / Paths / Resources
+readonly patina_path_resources_help="$patina_path_resources/help"
+
+# Patina / Paths / System / Files
+readonly system_os_release='/etc/os-release'
+readonly system_lsb_release='/etc/lsb-release'
+
+################################################################################
+# Global Functions #############################################################
+################################################################################
 
 patina_start() {
   # Ensure Patina is operating inside of a GNU/Linux environment
   if [ "$OSTYPE" != 'linux-gnu' ] ; then
-    echo_wrap "Patina does not currently support your operating system. For "`
-      `"more information, or to make a feature request, please visit "`
-      `"'$patina_metadata_url'."
+    echo_wrap "Patina does not currently support your operating system. For more information, or to make a feature request, please visit '$patina_metadata_url'."
     return
   fi
 
@@ -116,18 +114,15 @@ patina_start() {
     fi
   done
 
+  # Main Patina author/copyright header
+  echo_wrap "${patina_major_color}Patina v${patina_metadata_version} '${patina_metadata_codename}' / BASH v${BASH_VERSION%%[^0-9.]*}${color_reset}"
+  echo_wrap "${patina_major_color}Copyright (C) 2019 William Whinn${color_reset}"
+  echo_wrap "${patina_minor_color}$patina_metadata_url${color_reset}\\n"
+
   # Lock variables after Patina is successfully loaded
   readonly -a patina_components_list
   readonly TERM="$TERM"
   readonly OSTYPE="$OSTYPE"
-
-  # Main Patina author/copyright header
-  echo_wrap "${patina_major_color}Patina v${patina_metadata_version} "`
-    `"'${patina_metadata_codename}' / BASH "`
-    `"v${BASH_VERSION%%[^0-9.]*}${color_reset}"
-  echo_wrap "${patina_major_color}Copyright (C) 2019 William Whinn"`
-    `"${color_reset}"
-  echo_wrap "${patina_minor_color}$patina_metadata_url${color_reset}\\n"
 
   # Rubbish collection
   unset -f "${FUNCNAME[0]}"
@@ -145,62 +140,49 @@ patina_list_connected_components() {
   # Failure: Patina has been given too many arguments
   if [ "$#" -ge 1 ] ; then
     patina_throw_exception 'PE0002'
-    return
 
   # Success: At least one component has been connected
   elif [[ -n "${patina_components_list[*]}" ]] ; then
-    echo_wrap "\\nPatina has connected ${#patina_components_list[@]} "`
-      `"component(s)${color_reset}\\n"
+    echo_wrap "\\nPatina has connected ${#patina_components_list[@]} component(s)${color_reset}\\n"
 
     for component in "${patina_components_list[@]}" ; do
       echo_wrap "$(find "$component" -print0 | xargs -0 basename)"
     done
-
     echo
     return
 
   # Failure: No components have been connected
   else
     patina_throw_exception 'PE0007'
-    return
   fi
 }
 
 patina_throw_exception() {
   if [ "$#" -eq "0" ] ; then
     patina_throw_exception 'PE0001'
-    return
   elif [ "$#" -gt 1 ] ; then
     patina_throw_exception 'PE0002'
-    return
   elif [[ "$1" =~ [P][E][0-9][0-9][0-9][0-9] ]] ; then
     echo_wrap "${!1}"
     return
   else
     patina_throw_exception 'PE0000'
-    return
   fi
 }
 
 patina_open_folder() {
   if ( ! hash 'xdg-open' ) ; then
     patina_throw_exception 'PE0006'
-    return
   elif [ "$#" -eq "0" ] ; then
     patina_throw_exception 'PE0001'
-    return
   elif [ "$#" -gt 2 ] ; then
     patina_throw_exception 'PE0002'
-    return
   elif [ -n "$2" ] && [ "$2" != '-g' ] ; then
     patina_throw_exception 'PE0003'
-    return
   elif [ -f "$1" ] ; then
     patina_throw_exception 'PE0003'
-    return
   elif [ ! -d "$1" ] ; then
     patina_throw_exception 'PE0004'
-    return
   elif [ -d "$1" ] && [ "$2" = '-g' ] ; then
     cd "$1" || return
     xdg-open "$(pwd)" > /dev/null 2>&1
@@ -210,7 +192,6 @@ patina_open_folder() {
     return
   else
     patina_throw_exception 'PE0000'
-    return
   fi
 }
 
@@ -220,13 +201,10 @@ patina_open_folder_graphically() {
     return
   elif [ "$#" -gt 1 ] ; then
     patina_throw_exception 'PE0002'
-    return
   elif [ -f "$1" ] ; then
     patina_throw_exception 'PE0003'
-    return
   elif [ ! -d "$1" ] ; then
     patina_throw_exception 'PE0004'
-    return
   elif [ -d "$1" ] ; then
     patina_open_folder "$1" -g
     return
@@ -236,45 +214,38 @@ patina_open_folder_graphically() {
 echo_wrap() {
   if [ "$#" -eq "0" ] ; then
     patina_throw_exception 'PE0001'
-    return
   elif [ "$#" -gt 1 ] ; then
     patina_throw_exception 'PE0002'
-    return
   elif [ -n "$1" ] ; then
     (echo -e "$1") | fmt -w "$(tput cols)"
   else
     patina_throw_exception 'PE0000'
-    return
   fi
 }
 
 to_lower() {
   if [ "$#" -eq "0" ] ; then
     patina_throw_exception 'PE0001'
-    return
   elif [ "$#" -gt 1 ] ; then
     patina_throw_exception 'PE0002'
-    return
   elif [ -n "$1" ] ; then
     echo -e "$1" | tr '[:upper:]' '[:lower:]'
+    return
   else
     patina_throw_exception 'PE0000'
-    return
   fi
 }
 
 to_upper() {
   if [ "$#" -eq "0" ] ; then
     patina_throw_exception 'PE0001'
-    return
   elif [ "$#" -gt 1 ] ; then
     patina_throw_exception 'PE0002'
-    return
   elif [ -n "$1" ] ; then
     echo -e "$1" | tr '[:lower:]' '[:upper:]'
+    return
   else
     patina_throw_exception 'PE0000'
-    return
   fi
 }
 
@@ -294,18 +265,18 @@ patina_terminal_reset() {
   exec bash
 }
 
-###########
-# Exports #
-###########
+################################################################################
+# Global Function Exports ######################################################
+################################################################################
 
 export -f 'echo_wrap'
 export -f 'patina_throw_exception'
 export -f 'to_lower'
 export -f 'to_upper'
 
-###########
-# Aliases #
-###########
+################################################################################
+# Global Function Aliases ######################################################
+################################################################################
 
 # Patina / Core Commands
 alias 'p-help'='less $patina_path_root/README.md'
@@ -330,9 +301,9 @@ alias 'p-c-user'='patina_open_folder $patina_path_components_user'
 alias 'p-r'='patina_open_folder $patina_path_resources'
 alias 'p-r-help'='patina_open_folder $patina_path_resources_help'
 
-#############
-# Kickstart #
-#############
+################################################################################
+# Patina Kickstart #############################################################
+################################################################################
 
 patina_start
 
