a0065c234756980c617ed640a908dc204459b802
William Whinn <w.willis.1986@gmail.com>
2022-06-16 12:31:50 +0100

    Reformat code

--- components/system/patina_system_updater.sh
+++ components/system/patina_system_updater.sh
@@ -5,7 +5,7 @@
 ###########
 
 # Patina: A 'patina', 'layer', or 'toolbox' for BASH under Linux.
-# Copyright (C) 2021 William Willis Whinn
+# Copyright (C) 2022 William Willis Whinn
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,43 +20,27 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#########################
-# ShellCheck Directives #
-#########################
-
-# Override SC2154: "var is referenced but not assigned".
-# shellcheck disable=SC2154
-
 #############
 # Functions #
 #############
 
-# PATINA > FUNCTIONS > SYSTEM > PATINA UPDATER
-
 patina_update() {
-  # Failure: Package 'git' is not installed.
   if ( ! command -v 'git' > /dev/null 2>&1 ) ; then
     patina_raise_exception 'PE0006'
     return 127
-
-  # Failure: Patina root is not a 'git' repository.
   elif ( ! git -C "$PATINA_PATH_ROOT" rev-parse ) ; then
     patina_raise_exception 'PE0009'
     return 1
-
-  # Failure: Patina is not connected to the Internet.
   elif ( ! patina_detect_internet_connection ) ; then
     patina_raise_exception 'PE0008'
     return 1
 
-  # Success: Patina will check for newer 'git' commits.
-  elif ( git -C "$PATINA_PATH_ROOT" rev-parse ) ; then
-    cd "$PATINA_PATH_ROOT" || return 1
+  elif ( git -C "${PATINA_PATH_ROOT}" rev-parse ) ; then
+    cd "${PATINA_PATH_ROOT}" || return 1
     git pull
     cd ~- || return 1
     return 0
 
-  # Failure: Catch any other error condition here.
   else
     patina_raise_exception 'PE0000'
     return 1
@@ -66,8 +50,6 @@ patina_update() {
 ###########
 # Aliases #
 ###########
-
-# PATINA > FUNCTIONS > SYSTEM > PATINA UPDATER COMMANDS
 
 alias 'p-update'='patina_update'
 
