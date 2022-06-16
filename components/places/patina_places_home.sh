6895015a099cc309b3babf941b2db65d57d960b2
William Whinn <w.willis.1986@gmail.com>
2022-06-16 11:25:11 +0100

    Reformat code

--- components/places/patina_places_home.sh
+++ components/places/patina_places_home.sh
@@ -24,35 +24,35 @@
 # Variables #
 #############
 
-PATINA_PATH_HOME_DESKTOP="$HOME/Desktop"
+PATINA_PATH_HOME_DESKTOP="${HOME}/Desktop"
 readonly PATINA_PATH_HOME_DESKTOP
 export PATINA_PATH_HOME_DESKTOP
 
-PATINA_PATH_HOME_DOCUMENTS="$HOME/Documents"
+PATINA_PATH_HOME_DOCUMENTS="${HOME}/Documents"
 readonly PATINA_PATH_HOME_DOCUMENTS
 export PATINA_PATH_HOME_DOCUMENTS
 
-PATINA_PATH_HOME_DOWNLOADS="$HOME/Downloads"
+PATINA_PATH_HOME_DOWNLOADS="${HOME}/Downloads"
 readonly PATINA_PATH_HOME_DOWNLOADS
 export PATINA_PATH_HOME_DOWNLOADS
 
-PATINA_PATH_HOME_MUSIC="$HOME/Music"
+PATINA_PATH_HOME_MUSIC="${HOME}/Music"
 readonly PATINA_PATH_HOME_MUSIC
 export PATINA_PATH_HOME_MUSIC
 
-PATINA_PATH_HOME_PICTURES="$HOME/Pictures"
+PATINA_PATH_HOME_PICTURES="${HOME}/Pictures"
 readonly PATINA_PATH_HOME_PICTURES
 export PATINA_PATH_HOME_PICTURES
 
-PATINA_PATH_HOME_PUBLIC="$HOME/Public"
+PATINA_PATH_HOME_PUBLIC="${HOME}/Public"
 readonly PATINA_PATH_HOME_PUBLIC
 export PATINA_PATH_HOME_PUBLIC
 
-PATINA_PATH_HOME_TEMPLATES="$HOME/Templates"
+PATINA_PATH_HOME_TEMPLATES="${HOME}/Templates"
 readonly PATINA_PATH_HOME_TEMPLATES
 export PATINA_PATH_HOME_TEMPLATES
 
-PATINA_PATH_HOME_VIDEOS="$HOME/Videos"
+PATINA_PATH_HOME_VIDEOS="${HOME}/Videos"
 readonly PATINA_PATH_HOME_VIDEOS
 export PATINA_PATH_HOME_VIDEOS
 
@@ -60,15 +60,15 @@ export PATINA_PATH_HOME_VIDEOS
 # Aliases #
 ###########
 
-alias 'home'='patina_open_folder $HOME'
+alias 'home'='patina_open_folder ${HOME}'
 
-alias 'desktop'='patina_open_folder "$PATINA_PATH_HOME_DESKTOP"'
-alias 'documents'='patina_open_folder "$PATINA_PATH_HOME_DOCUMENTS"'
-alias 'downloads'='patina_open_folder "$PATINA_PATH_HOME_DOWNLOADS"'
-alias 'music'='patina_open_folder "$PATINA_PATH_HOME_MUSIC"'
-alias 'pictures'='patina_open_folder "$PATINA_PATH_HOME_PICTURES"'
-alias 'public'='patina_open_folder "$PATINA_PATH_HOME_PUBLIC"'
-alias 'templates'='patina_open_folder "$PATINA_PATH_HOME_TEMPLATES"'
-alias 'videos'='patina_open_folder "$PATINA_PATH_HOME_VIDEOS"'
+alias 'desktop'='patina_open_folder "${PATINA_PATH_HOME_DESKTOP}"'
+alias 'documents'='patina_open_folder "${PATINA_PATH_HOME_DOCUMENTS}"'
+alias 'downloads'='patina_open_folder "${PATINA_PATH_HOME_DOWNLOADS}"'
+alias 'music'='patina_open_folder "{$PATINA_PATH_HOME_MUSIC}"'
+alias 'pictures'='patina_open_folder "${PATINA_PATH_HOME_PICTURES}"'
+alias 'public'='patina_open_folder "${PATINA_PATH_HOME_PUBLIC}"'
+alias 'templates'='patina_open_folder "${PATINA_PATH_HOME_TEMPLATES}"'
+alias 'videos'='patina_open_folder "${PATINA_PATH_HOME_VIDEOS}"'
 
 # End of File.
