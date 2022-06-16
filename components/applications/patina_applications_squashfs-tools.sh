9045c0014e1c9839f4ee67f89cfd87595ec40d4a
William Whinn <w.willis.1986@gmail.com>
2022-06-16 10:47:54 +0100

    Reformat code

--- components/applications/patina_applications_squashfs-tools.sh
+++ components/applications/patina_applications_squashfs-tools.sh
@@ -24,17 +24,13 @@
 # References #
 ##############
 
-# GitHub / Create an encrypted DVD with SquashFS and LUKS
 # - https://tinyurl.com/yu4zjkv6
 
 #############
 # Functions #
 #############
 
-# PATINA > FUNCTIONS > APPLICATIONS > SQUASHFS-TOOLS
-
 patina_squashfs-tools() {
-  # Success: Display help and exit.
   if [ "$1" = '--help' ] ; then
     echo "Usage: p-squash [DIRECTORY] [OPTION]"
     echo "Create a read-only disk image in SquashFS format."
@@ -44,39 +40,26 @@ patina_squashfs-tools() {
     echo -e "  --help\\tDisplay this help and exit."
     echo
     return 0
-
-  # Failure: Command 'mksquashfs' is not available.
   elif ( ! command -v 'mksquashfs' > /dev/null 2>&1 ) ; then
     patina_raise_exception 'PE0006'
     patina_required_software 'mksquashfs' 'squashfs-tools'
     return 127
-
-  # Failure: Patina has not been given an argument.
   elif [ "$#" -eq 0 ] ; then
     patina_raise_exception 'PE0001'
     return 1
-
-  # Failure: Patina has been given too many arguments.
   elif [ "$#" -gt 2 ] ; then
     patina_raise_exception 'PE0002'
     return 1
-
-  # Failure: The target exists, but is a file.
   elif [ -f "$1" ] ; then
     patina_raise_exception 'PE0014'
     return 1
-
-  # Failure: The target directory does not exist.
   elif [ ! -d "$1" ] ; then
     patina_raise_exception 'PE0004'
     return 1
-
-  # Failure: The target disk image already exists.
   elif [ -f "$(basename "$1").squashfs" ] ; then
     patina_raise_exception 'PE0011'
     return 1
 
-  # Success: Create LUKS encrypted SquashFS image.
   elif [ -d "$1" ] && [ "$2" = '--enc' ] ; then
     mksquashfs "$1" "$1.luks.squashfs"
 
@@ -94,12 +77,9 @@ patina_squashfs-tools() {
 
     return 0
 
-  # Success: Create SquashFS disk image.
   elif [ -d "$1" ] ; then
     mksquashfs "$1" "$1.squashfs"
     return 0
-
-  # Failure: Catch all.
   else
     patina_raise_exception 'PE0000'
     return 1
@@ -110,15 +90,11 @@ patina_squashfs-tools() {
 # Exports #
 ###########
 
-# PATINA > FUNCTIONS > APPLICATIONS > SQUASHFS-TOOLS
-
 export -f 'patina_squashfs-tools'
 
 ###########
 # Aliases #
 ###########
-
-# PATINA > FUNCTIONS > APPLICATIONS > SQUASHFS-TOOLS COMMANDS
 
 alias 'p-squash'='patina_squashfs-tools'
 
