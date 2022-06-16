dc86fd118e594339f7d539f23885fcf2ce811611
William Whinn <w.willis.1986@gmail.com>
2022-06-16 10:38:37 +0100

    Reformat code

--- components/applications/patina_applications_genisoimage.sh
+++ components/applications/patina_applications_genisoimage.sh
@@ -24,10 +24,7 @@
 # Functions #
 #############
 
-# PATINA > FUNCTIONS > APPLICATIONS > GENISOIMAGE
-
 patina_genisoimage() {
-  # Success: Display help and exit.
   if [ "$1" = '--help' ] ; then
     echo "Usage: p-iso [DIRECTORY] [OPTION]"
     echo "Create a read-only disk image in .iso format."
@@ -37,44 +34,30 @@ patina_genisoimage() {
     echo -e "  --help\\tDisplay this help and exit."
     echo
     return 0
-
-  # Failure: Command 'mkisofs' is not available.
   elif ( ! command -v 'mkisofs' > /dev/null 2>&1 ) ; then
     patina_raise_exception 'PE0006'
     patina_required_software 'mkisofs' 'genisoimage'
     return 127
-
-  # Failure: Patina has not been given an argument.
   elif [ "$#" -eq 0 ] ; then
     patina_raise_exception 'PE0001'
     return 1
-
-  # Failure: Patina has been given too many arguments.
   elif [ "$#" -gt 2 ] ; then
     patina_raise_exception 'PE0002'
     return 1
-
-  # Failure: A valid argument was not provided.
   elif [ -n "$2" ] && [ "$2" != '--udf' ] ; then
     patina_raise_exception 'PE0001'
     return 1
-
-  # Failure: The target exists, but is a file.
   elif [ -f "$1" ] ; then
     patina_raise_exception 'PE0014'
     return 1
-
-  # Failure: The target directory does not exist.
   elif [ ! -d "$1" ] ; then
     patina_raise_exception 'PE0004'
     return 1
-
-  # Failure: The target disk image already exists.
   elif [ -f "$(basename "$1").iso" ] ; then
     patina_raise_exception 'PE0011'
     return 1
 
-  # Success: Create ISO Disk Image (ISO-9660 compliant).
+  # Create ISO Disk Image (ISO-9660 compliant).
   elif [ -d "$1" ] && [ -z "$2" ] ; then
     mkisofs -volid "$(generate_volume_label)" -output "$(basename "$1").iso" \
       -input-charset UTF-8 \
@@ -84,7 +67,7 @@ patina_genisoimage() {
       "$1"
     return 0
 
-  # Success: Create ISO Disk Image (Non ISO-9660 compliant).
+  # Create ISO Disk Image (UDF, Non ISO-9660 compliant).
   elif [ -d "$1" ] && [ "$2" = '--udf' ] ; then
     mkisofs -volid "$(generate_volume_label)" -output "$(basename "$1").iso" \
       -input-charset UTF-8 \
@@ -95,7 +78,6 @@ patina_genisoimage() {
       "$1"
     return 0
 
-  # Failure: Catch all.
   else
     patina_raise_exception 'PE0000'
     return 1
@@ -106,15 +88,11 @@ patina_genisoimage() {
 # Exports #
 ###########
 
-# PATINA > FUNCTIONS > APPLICATIONS > GENISOIMAGE
-
 export -f 'patina_genisoimage'
 
 ###########
 # Aliases #
 ###########
-
-# PATINA > FUNCTIONS > APPLICATIONS > GENISOIMAGE COMMANDS
 
 alias 'p-iso'='patina_genisoimage'
 
