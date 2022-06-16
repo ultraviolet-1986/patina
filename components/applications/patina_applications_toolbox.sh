14bdb8fbad6588fa0155df4f2e925540aadcf7ab
William Whinn <w.willis.1986@gmail.com>
2022-06-16 10:53:21 +0100

    Reformat code

--- components/applications/patina_applications_toolbox.sh
+++ components/applications/patina_applications_toolbox.sh
@@ -24,20 +24,16 @@
 # References #
 ##############
 
-# Fedora Magazine / Backup and restore Toolboxes
 # - https://tinyurl.com/tru8y8jz
 
 #############
 # Functions #
 #############
 
-# PATINA > FUNCTIONS > APPLICATIONS > TOOLBOX
-
 patina_toolbox(){
   local datestamp
   datestamp="$(date --utc +%Y%m%d_%H%M%S)"
 
-  # Success: Display contents of help file.
   if [ "$1" = '--help' ] ; then
     echo "Usage: p-toolbox [OPTION] [CONTAINER/IMAGE/ARCHIVE]"
     echo "Easily manage 'toolbox' containers."
@@ -50,34 +46,23 @@ patina_toolbox(){
     echo -e "  --help\\tDisplay this help and exit."
     echo
     return 0
-
-  # Failure: Command 'toolbox' is not available.
   elif ( ! command -v 'toolbox' > /dev/null 2>&1 ) ; then
     patina_raise_exception 'PE0006'
     patina_required_software 'toolbox' 'toolbox'
     return 127
-
-  # Failure: Command 'podman' is not available.
   elif ( ! command -v 'podman' > /dev/null 2>&1 ) ; then
     patina_raise_exception 'PE0006'
     return 127
-
-  # Failure: Patina has not been given an argument.
   elif [ "$#" -eq 0 ] ; then
     patina_raise_exception 'PE0001'
     return 1
-
-  # Failure: Patina has been given too many arguments.
   elif [ "$#" -gt 2 ] ; then
     patina_raise_exception 'PE0002'
     return 1
-
-  # Failure: A container name was not provided.
   elif [ -z "$2" ] ; then
     patina_raise_exception 'PE0001'
     return 1
 
-  # Success: Commit a container to a date-stamped image.
   elif [ "$1" = 'commit' ] ; then
     local image_name="$2_${datestamp}"
     local container="${YELLOW}${2}${COLOR_RESET}"
@@ -88,7 +73,6 @@ patina_toolbox(){
 
     return 0
 
-  # Success: Export a given image to an archive.
   elif [ "$1" = 'export' ] ; then
     local image="${YELLOW}${2}${COLOR_RESET}"
     local file="${YELLOW}${2}.tar${COLOR_RESET}"
@@ -98,26 +82,20 @@ patina_toolbox(){
 
     return 0
 
-  # Success: Import a given archive to an image.
   elif [ "$1" = 'import' ] && [ -f "$2" ] ; then
     local file="${YELLOW}${2}${COLOR_RESET}"
     local image
     image="${YELLOW}$(basename "$2" .tar)${COLOR_RESET}"
 
-    # Success: A supported file type was provided.
     if [ "${2: -4}" == ".tar" ] ; then
       echo -e "Now importing archive ${file} to image ${image}. Please wait."
       podman load -i "$2" || return 1
-
       return 0
-
-    # Failure: An unsupported file type was specified.
     else
       patina_raise_exception 'PE0017'
       return 1
     fi
 
-  # Failure: Catch all.
   else
     patina_raise_exception 'PE0000'
     return 1
@@ -128,15 +106,11 @@ patina_toolbox(){
 # Exports #
 ###########
 
-# PATINA > FUNCTIONS > APPLICATIONS > TOOLBOX
-
 export -f 'patina_toolbox'
 
 ###########
 # Aliases #
 ###########
-
-# PATINA > FUNCTIONS > APPLICATIONS > TOOLBOX COMMANDS
 
 alias 'p-toolbox'="patina_toolbox"
 
