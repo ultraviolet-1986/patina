e4dce2ed89bb0f559f8e391e09513aa245ef5ea4
William Whinn <w.willis.1986@gmail.com>
2022-06-16 10:51:09 +0100

    Reformat code

--- components/applications/patina_applications_timeshift.sh
+++ components/applications/patina_applications_timeshift.sh
@@ -24,10 +24,7 @@
 # Functions #
 #############
 
-# PATINA > FUNCTIONS > APPLICATIONS > TIMESHIFT
-
 patina_timeshift() {
-  # Success: Display help and exit.
   if [ "$1" = '--help' ] ; then
     echo "Usage: p-timeshift [OPTION]"
     echo "Quickly manage system snapshots using Timeshift."
@@ -38,35 +35,22 @@ patina_timeshift() {
     echo -e "  --help\\tDisplay this help and exit."
     echo
     return 0
-
-  # Failure: Patina cannot detect a required application.
   elif ( ! command -v 'timeshift' > /dev/null 2>&1 ) ; then
     patina_raise_exception 'PE0006'
     patina_required_software 'timeshift' 'timeshift'
     return 127
-
-  # Failure: Patina has not been given an argument.
   elif [ "$#" -eq 0 ] ; then
     patina_raise_exception 'PE0001'
     return 1
-
-  # Failure: Patina has been given too many arguments.
   elif [ "$#" -gt 1 ] ; then
     patina_raise_exception 'PE0002'
     return 1
-
-  # Success: Create a snapshot of the system and provide a default
-  # label.
   elif [ "$1" = 'create' ] ; then
-    sudo timeshift --create --yes --comment "System Checkpoint (Patina)." --scripted
+    sudo timeshift --create --yes --comment "System Checkpoint (Patina)" --scripted
     return 0
-
-  # Success: User will be prompted to restore a specific snapshot.
   elif [ "$1" = 'restore' ] ; then
     sudo timeshift --restore
     return 0
-
-  # Failure: Catch all.
   else
     patina_raise_exception 'PE0000'
     return 1
@@ -76,8 +60,6 @@ patina_timeshift() {
 ###########
 # Aliases #
 ###########
-
-# PATINA > FUNCTIONS > APPLICATIONS > TIMESHIFT COMMANDS
 
 alias 'p-timeshift'='patina_timeshift'
 
